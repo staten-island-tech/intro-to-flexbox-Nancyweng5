be643f397a9f0befe49b02eac71f0bfd90b98302
Nancyweng5 <wengalex55@gmail.com>
2025-10-08 12:32:21 -0400

    ,

---
+++ l.js
@@ -0,0 +1,14 @@
+const sneakers = [
+    {name : "Jordan 700s"}
+    price = 1.
+]
+
+
+const genshin = [
+    { name : "", Price:__, instock:true, brand : Genshin, img: ___, alt:___0
+
+    }
+]
+
+sneakers.forEach((genshin)=> makeCard(Genshin));
+function makeCard(item){}
---
+++ merch.js
@@ -0,0 +1,125 @@
+
+const products = [
+  {
+    img: "wa.webp",
+    name: "Wanderer Impression Brooch",
+    price: "$49.90",
+    link: "https://genshinfans.com/products/official-wanderer-impression-brooch",
+  },
+  {
+    img: "Klee.webp",
+    name: "Klee Bomb Sling Water Bottle",
+    price: "$57.90",
+    link: "https://genshinfans.com/products/official-klee-bomb-sling-water-bottle",
+  },
+  {
+    img: "zhongli.webp",
+    name: "Zhongli Impression Series Crystal Aromatherapy Gift Box",
+    price: "$129.90",
+    link: "https://genshinfans.com/products/official-zhongli-crystal-aromatherapy-gift-box",
+  },
+  {
+    img: "Official-Yae-Miko-Bracelet.webp",
+    name: "Yae Miko Impression Bracelet/Necklace",
+    price: "$49.90",
+    link: "https://genshinfans.com/products/official-miko-bracelet-necklace",
+  },
+  {
+    img: "raiden.webp",
+    name: "The Statue of Her Excellency Raiden Shogun",
+    price: "$99.90",
+    link: "https://genshinfans.com/products/official-the-statue-of-her-excellency-raiden-shogun",
+  },
+  {
+    img: "childe.webp",
+    name: "Resplendent Feast Tartaglia Merchandise",
+    price: "$219.90",
+    link: "https://genshinfans.com/products/genshin-resplendent-feast-tartaglia-merch",
+  },
+  {
+    img: "ZT01_c02358e5-66ab-42ea-99ee-822358617b7e.webp",
+    name: "Furina Sweet Afternoon Ode Chibi Figure",
+    price: "$109.90",
+    link: "https://genshinfans.com/products/official-furina-chibi-figure",
+  },
+  {
+    img: "Kazuha.webp",
+    name: "Kaedehara Kazuha Action Figure with Clothes and Accessories",
+    price: "$169.90",
+    link: "https://genshinfans.com/products/official-kazuha-action-figure-with-clothes",
+  },
+  {
+    img: "n.webp",
+    name: "Neuvillette Afternoon Tea Cup Set",
+    price: "$89.90",
+    link: "https://genshinfans.com/products/official-neuvillette-afternoon-tea-cup-set",
+  },
+  {
+    img: "a.webp",
+    name: "Arlecchino Impression Crystal Aromatherapy Gift Box",
+    price: "$119.90",
+    link: "https://genshinfans.com/products/official-arlecchino-crystal-aromatherapy-gift-box",
+  },
+  {
+    img: "v.webp",
+    name: "Venti Impression Envelope Bag & Tote Bag",
+    price: "$114.90",
+    link: "https://genshinfans.com/products/official-venti-clutch-tote-bag",
+  },
+  {
+    img: "al.webp",
+    name: "Alhaitham Impression Travel Size Perfume Gift Set",
+    price: "$74.90",
+    link: "https://genshinfans.com/products/official-alhaitham-travel-size-perfume-gift-set",
+  },
+  {
+    img: "di.webp",
+    name: "Diluc Impression Messenger Bag",
+    price: "$111.90",
+    link: "https://genshinfans.com/products/official-diluc-messenger-bag",
+  },
+  {
+    img: "xiao.webp",
+    name: "Genshin Impact Xiao Vigilant Yaksha Ver. 1/7 Scale Figure",
+    price: "$259.90",
+    link: "https://genshinfans.com/products/official-xiao-vigilant-1-7-scale-figure",
+  },
+  {
+    img: "hutao.webp",
+    name: "Hu Tao Chibi Happy Shake Figure Stand",
+    price: "$29.90",
+    link: "https://genshinfans.com/products/official-hu-tao-happy-shake-ornament",
+  },
+  {
+    img: "Nahida.webp",
+    name: "Nahida Impression Hairpin",
+    price: "$38.90",
+    link: "https://genshinfans.com/products/official-merchandise-nahida-impression-hairpin",
+  },
+  {
+    img: "wrio.webp",
+    name: "Genshin Impact Wriothesley Action Figure Doll",
+    price: "$169.90",
+    link: "https://genshinfans.com/products/official-wriothesley-action-figure",
+  },
+  {
+    img: "kinich.webp",
+    name: "Genshin Impact Kinich Theme Impression Double Layer Short Necklace",
+    price: "$69.90",
+    link: "https://genshinfans.com/products/official-kinich-double-layer-necklace",
+  },
+  {
+    img: "citlali.webp",
+    name: "Genshin Impact Citlali Obsidian Opalstar Ver. 1/8 Scale Figure",
+    price: "$109.90",
+    link: "https://genshinfans.com/products/official-genshin-citlali-1-8-figure",
+  },
+  {
+    img: "albedo.webp",
+    name: "Genshin Impact Albedo Theme Impression Brooch",
+    price: "$52.90",
+    link: "https://genshinfans.com/products/official-albedo-theme-impression-brooch",
+  },
+];
+
+products.forEach((product) => makeCaed(product))
