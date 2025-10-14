f6ff3cf86aaf15ee8ce65a190707aac7fc1633d0
Nancyweng5 <wengalex55@gmail.com>
2025-10-14 12:33:04 -0400

    .

--- merch.js
+++ merch.js
@@ -1,181 +1,192 @@
-
 const products = [
   {
     img: "wa.webp",
     name: "Wanderer Impression Brooch",
-    inStock : true,
+    inStock: true,
     catagory: "accesories",
-    price: 49.90,
+    price: 49.9,
     link: "https://genshinfans.com/products/official-wanderer-impression-brooch",
   },
   {
     img: "Klee.webp",
     name: "Klee Bomb Sling Water Bottle",
-    inStock : true,
+    inStock: true,
     catagory: "accesories",
-    price: 57.90,
+    price: 57.9,
     link: "https://genshinfans.com/products/official-klee-bomb-sling-water-bottle",
   },
   {
     img: "zhongli.webp",
     name: "Zhongli Impression Series Crystal Aromatherapy Gift Box",
     catagory: "accesories",
-    inStock : false,
-    price: 129.90,
+    inStock: false,
+    price: 129.9,
     link: "https://genshinfans.com/products/official-zhongli-crystal-aromatherapy-gift-box",
   },
   {
     img: "Official-Yae-Miko-Bracelet.webp",
     name: "Yae Miko Impression Bracelet/Necklace",
     catagory: "accesories",
-    inStock : true,
-    price: 49.90,
+    inStock: true,
+    price: 49.9,
     link: "https://genshinfans.com/products/official-miko-bracelet-necklace",
   },
   {
     img: "raiden.webp",
     name: "The Statue of Her Excellency Raiden Shogun",
     catagory: "figures",
-    inStock : true,
-    price: 99.90,
+    inStock: true,
+    price: 99.9,
     link: "https://genshinfans.com/products/official-the-statue-of-her-excellency-raiden-shogun",
   },
   {
     img: "childe.webp",
     name: "Resplendent Feast Tartaglia Merchandise",
     catagory: "accesories",
-    inStock : true,
-    price: 219.90,
+    inStock: true,
+    price: 219.9,
     link: "https://genshinfans.com/products/genshin-resplendent-feast-tartaglia-merch",
   },
   {
     img: "ZT01_c02358e5-66ab-42ea-99ee-822358617b7e.webp",
     name: "Furina Sweet Afternoon Ode Chibi Figure",
     catagory: "figures",
-    inStock : false, 
-    price: 109.90,
+    inStock: false,
+    price: 109.9,
     link: "https://genshinfans.com/products/official-furina-chibi-figure",
   },
   {
     img: "Kazuha.webp",
     name: "Kaedehara Kazuha Action Figure with Clothes and Accessories",
     catagory: "figures",
-    inStock: false, 
-    price: 169.90,
+    inStock: false,
+    price: 169.9,
     link: "https://genshinfans.com/products/official-kazuha-action-figure-with-clothes",
   },
   {
     img: "n.webp",
     name: "Neuvillette Afternoon Tea Cup Set",
     catagory: "accesories",
-    inStock : true,
-    price: 89.90,
+    inStock: true,
+    price: 89.9,
     link: "https://genshinfans.com/products/official-neuvillette-afternoon-tea-cup-set",
   },
   {
     img: "a.webp",
     name: "Arlecchino Impression Crystal Aromatherapy Gift Box",
     catagory: "accesories",
-    inStock : true,
-    price: 119.90,
+    inStock: true,
+    price: 119.9,
     link: "https://genshinfans.com/products/official-arlecchino-crystal-aromatherapy-gift-box",
   },
   {
     img: "v.webp",
     name: "Venti Impression Envelope Bag & Tote Bag",
     catagory: "clothing",
-    inStock : true,
-    price: 114.90,
+    inStock: true,
+    price: 114.9,
     link: "https://genshinfans.com/products/official-venti-clutch-tote-bag",
   },
   {
     img: "al.webp",
     name: "Alhaitham Impression Travel Size Perfume Gift Set",
     catagory: "accesories",
-    inStock : true,
-    price: 74.90,
+    inStock: true,
+    price: 74.9,
     link: "https://genshinfans.com/products/official-alhaitham-travel-size-perfume-gift-set",
   },
   {
     img: "di.webp",
     name: "Diluc Impression Messenger Bag",
     catagory: "clothing",
-    inStock : true,
-    price: 111.90,
+    inStock: true,
+    price: 111.9,
     link: "https://genshinfans.com/products/official-diluc-messenger-bag",
   },
   {
     img: "xiao.webp",
     name: "Genshin Impact Xiao Vigilant Yaksha Ver. 1/7 Scale Figure",
     catagory: "figures",
-    inStock : true,
-    price: 259.90,
+    inStock: true,
+    price: 259.9,
     link: "https://genshinfans.com/products/official-xiao-vigilant-1-7-scale-figure",
   },
   {
     img: "hutao.webp",
     name: "Hu Tao Chibi Happy Shake Figure Stand",
     catagory: "figures",
-    inStock : false,
-    price: 29.90,
+    inStock: false,
+    price: 29.9,
     link: "https://genshinfans.com/products/official-hu-tao-happy-shake-ornament",
   },
   {
     img: "Nahida.webp",
     name: "Nahida Impression Hairpin",
     catagory: "accesories",
-    inStock : true,
-    price: 38.90,
+    inStock: true,
+    price: 38.9,
     link: "https://genshinfans.com/products/official-merchandise-nahida-impression-hairpin",
   },
   {
     img: "wrio.webp",
     name: "Genshin Impact Wriothesley Action Figure Doll",
     catagory: "figures",
-    inStock : false,
-    price: 169.90,
+    inStock: false,
+    price: 169.9,
     link: "https://genshinfans.com/products/official-wriothesley-action-figure",
   },
   {
     img: "kinich.webp",
     name: "Genshin Impact Kinich Theme Impression Double Layer Short Necklace",
     catagory: "accesories",
-    inStock : true,
-    price: 69.90,
+    inStock: true,
+    price: 69.9,
     link: "https://genshinfans.com/products/official-kinich-double-layer-necklace",
   },
   {
     img: "citlali.webp",
     name: "Genshin Impact Citlali Obsidian Opalstar Ver. 1/8 Scale Figure",
     catagory: "figures",
-    inStock : false,
-    price: 109.90,
+    inStock: false,
+    price: 109.9,
     link: "https://genshinfans.com/products/official-genshin-citlali-1-8-figure",
   },
   {
     img: "albedo.webp",
     name: "Genshin Impact Albedo Theme Impression Brooch",
     catagory: "accesories",
-    inStock : true,
-    price: 52.90,
+    inStock: true,
+    price: 52.9,
     link: "https://genshinfans.com/products/official-albedo-theme-impression-brooch",
   },
 ];
 
 //create inject function
-function inject(item){
-  //do something
-  let name = "products";
+function inject(item) {
   const container = document.querySelector(".container");
-  container.indertAdjacentHTML("afterbegin", `<h1>${product.name}</h1>`);
+
+  container.insertAdjacentHTML("afterbegin", `<div class="card">
+        <img src=${products.img}" alt="." />
+        <h2>${products.name}</h2>
+        <h3>${products.price}</h3>
+        <a
+          ${products.link}
+        >
+          <button>Purchase</button>
+        </a>
+      </div>`);
   //query the container
 }
-
-/* inject(products[0])
-inject(products[0])
-inject(products[0])
-inject(products[0]) */
 //loop through items
-
 products.forEach((product) => inject(product));
-//for every product we 
+//for every product we
+
+function addToCard() {
+  const buttons = document.querySelectorAll("button");
+  const btnarray = Array.from(buttons);
+  btnarray.forEach((btn) =>
+    btn.addEventListerner("click", function (event) {
+      console.log(event.target.textContent);
+    })
+  );
+}
